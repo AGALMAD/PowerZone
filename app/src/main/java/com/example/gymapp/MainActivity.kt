574ea6af71cfe229b6904c77b6dff9fb8f0d9edd
AGALMAD <118524298+AGALMAD@users.noreply.github.com>
2024-10-29 20:28:22 +0100

    Nuevo contenido en about us

--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -26,105 +27,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                AboutUs()
-
+                AboutUsContent()
             }
         }
     }
-}
-
-@Composable
-fun AboutUs() {
-    val context = LocalContext.current
-
-
-
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background), //!!!!!! Importante para que cambie el color de fondo
-        verticalArrangement = Arrangement.spacedBy(18.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        //Titulo sobre nosotros
-        Text(
-            text = context.getString(R.string.about_us_title),
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.titleLarge
-
-        )
-
-        //Imagen Principal
-        addImageLogo()
-
-        //Texto sobre nosotros
-        Text(
-            text = context.getString(R.string.about_us_text),
-            textAlign = TextAlign.Justify,
-            color = MaterialTheme.colorScheme.secondary,
-            style = MaterialTheme.typography.titleLarge,
-
-        )
-
-
-        //Titulo colaboradores
-        Text(
-            text = context.getString(R.string.collaborators_title),
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.titleLarge
-
-        )
-
-        addImageAle()
-
-        Image(
-            painter = painterResource(R.drawable.dani),
-            contentDescription = null
-        )
-        addImageDani()
-
-
-    }
-
-}
-
-
-@Composable
-fun addImageLogo() {
-
-    val image = painterResource(R.drawable.logo_sin_fondo)
-
-    Image(
-        painter = image,
-        contentDescription = null
-    )
-
-}
-
-@Composable
-fun addImageAle() {
-
-    val image = painterResource(R.drawable.foto_perfil)
-
-    Image(
-        painter = image,
-        contentDescription = null
-    )
-
-}
-
-
-@Composable
-fun addImageDani() {
-
-    val image = painterResource(R.drawable.dani)
-
-    Image(
-        painter = image,
-        contentDescription = null
-    )
-
 }
 
