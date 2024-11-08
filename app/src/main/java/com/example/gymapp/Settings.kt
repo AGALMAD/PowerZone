298c3ccad964f1b7f7f631348cd9b3f4d3c24485
dcuecal515 <163828595+dcuecal515@users.noreply.github.com>
2024-11-08 20:20:29 +0100

    Creado los settings (sin terminar)

--- app/src/main/java/com/example/gymapp/Settings.kt
+++ app/src/main/java/com/example/gymapp/Settings.kt
@@ -1,2 +1,94 @@
 package com.example.gymapp
 
+import android.os.Bundle
+import android.widget.RadioButton
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.compose.AppTheme
+
+class Settings : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                SettingsContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun SettingsContent(){
+
+    var radioButtonCheck by remember { mutableIntStateOf(1) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.spacedBy(18.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Titulo configuración",
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.displayLarge,
+            modifier = Modifier.padding(30.dp)
+        )
+        Image(
+            painter = if(isSystemInDarkTheme() )
+                painterResource(R.drawable.logo_sin_fondo_blanco)
+            else
+                painterResource(R.drawable.logo_sin_fondo_negro),
+            contentDescription = "Imagen Logo principal",
+            modifier = Modifier.size(150.dp)
+        )
+        Text(
+            text = "Seleccione la zona a entrenar:",
+            textAlign = TextAlign.Justify,
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(15.dp)
+        )
+
+    }
+}
+
+fun GenerateRadioButton(
+    textContent: String,
+    id: Int,
+
+){
+    Row() {
+        RadioButton(
+            selected = isSelected
+        )
+    }
+
+}
