1ba9a4f968959c6d808a8d9ad4031997be3593b0
AGALMAD <agalmad@g.educaand.es>
2024-11-26 10:30:23 +0100

    Cambios en aspecto

--- Frontend/app/src/main/java/com/example/gymapp/Views/BodyPartsContent.kt
+++ Frontend/app/src/main/java/com/example/gymapp/Views/BodyPartsContent.kt
@@ -1,5 +1,6 @@
 package com.example.gymapp.Views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,6 +27,8 @@ import coil3.request.crossfade
 import com.example.ViewModels.BodyPartsViewModel
 import com.example.gymapp.R
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.example.compose.backgroundDark
 
 @Composable
 fun BodyPartsContent(navController: NavHostController, viewModel: BodyPartsViewModel) {
@@ -33,12 +38,13 @@ fun BodyPartsContent(navController: NavHostController, viewModel: BodyPartsViewM
         viewModel.fetchBodyParts()
     }
 
-    Column {
+    Column (modifier = Modifier.background(MaterialTheme.colorScheme.background)){
         if (bodyParts.isEmpty()) {
-            // Show loading indicator or placeholder
-            Text(text = "Loading...")
+            // Mustra una barra circular mientras cargan las imágenes
+            CircularProgressIndicator()
         } else {
             // Mostrar la lista de partes del cuerpo
+            // carga los elementos visibles
             LazyColumn {
                 items(bodyParts) { bodyPart ->
                     Column(
@@ -48,7 +54,12 @@ fun BodyPartsContent(navController: NavHostController, viewModel: BodyPartsViewM
                     ) {
                         Text(
                             text = bodyPart.title,
-                            modifier = Modifier.padding(bottom = 4.dp)
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.displayLarge,
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 4.dp)
                         )
                         AsyncImage(
                             model = ImageRequest.Builder(LocalContext.current)
