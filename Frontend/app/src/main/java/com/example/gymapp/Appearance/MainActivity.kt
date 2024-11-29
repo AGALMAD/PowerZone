a38bc70249adc8a253edd9bdedd27062f309a93e
AGALMAD <118524298+AGALMAD@users.noreply.github.com>
2024-11-29 16:29:31 +0100

    Vista de cuenta del usuario

--- Frontend/app/src/main/java/com/example/gymapp/Appearance/MainActivity.kt
+++ Frontend/app/src/main/java/com/example/gymapp/Appearance/MainActivity.kt
@@ -17,6 +17,7 @@ import com.example.gymapp.Appearance.Views.Api.BodyPartsContent
 import com.example.gymapp.Appearance.Views.Api.ExercisesContent
 import com.example.gymapp.Appearance.Views.Authentication.Login
 import com.example.gymapp.Appearance.Views.Authentication.Register
+import com.example.gymapp.Appearance.Views.Authentication.UserAccount
 import com.example.gymapp.Appearance.Views.SettingsContent
 import com.example.gymapp.GymApi.ViewModels.AuthViewModel
 import com.example.gymapp.GymApi.ViewModels.ExercisesViewModel
@@ -42,6 +43,8 @@ class MainActivity : ComponentActivity() {
                     composable(Routes.ExercisesScreen.route) { ExercisesContent(navController, exercisesViewModel) }
                     composable(Routes.Login.route) { Login(navController, authViewModel) }
                     composable(Routes.Register.route) { Register(navController, authViewModel) }
+                    composable(Routes.UserAccount.route) { UserAccount(navController, authViewModel) }
+
                 }
             }
         }
