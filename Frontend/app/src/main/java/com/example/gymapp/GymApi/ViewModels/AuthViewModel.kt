87875ad4245b4f8e62f96ed1cae3ea394ee82a1d
AGALMAD <agalmad@g.educaand.es>
2024-11-29 12:01:21 +0100

    métodos para inicio de sesión y registro

--- Frontend/app/src/main/java/com/example/gymapp/GymApi/ViewModels/AuthViewModel.kt
+++ Frontend/app/src/main/java/com/example/gymapp/GymApi/ViewModels/AuthViewModel.kt
@@ -1,6 +1,82 @@
 package com.example.gymapp.GymApi.ViewModels
 
+import android.annotation.SuppressLint
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.gymapp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class AuthViewModel : ViewModel() {
+
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val _authState = MutableLiveData<AuthState>()
+    val authState: LiveData<AuthState> = _authState
+
+
+    fun checkAuthStatus(){
+        if (auth.currentUser == null){
+            _authState.value = AuthState.Unauthenticated
+        }
+        else {
+            _authState.value = AuthState.Authenticated
+        }
+    }
+
+
+    fun login(email : String, password : String){
+
+        if (email.isEmpty() || password.isEmpty()){
+            _authState.value = AuthState.Error("Email or passord can´t be empty")
+        }
+
+        _authState.value = AuthState.Loading
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                }
+                else{
+                    _authState.value = AuthState.Error(task.exception?.message?:"Something went wrong" )
+                }
+            }
+    }
+
+
+
+    fun singup(email : String, password : String){
+
+        if (email.isEmpty() || password.isEmpty()){
+            _authState.value = AuthState.Error("Email or passord can´t be empty")
+        }
+
+        _authState.value = AuthState.Loading
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    _authState.value = AuthState.Authenticated
+                }
+                else{
+                    _authState.value = AuthState.Error(task.exception?.message?:"Something went wrong" )
+                }
+            }
+    }
+
+
+    fun singout(){
+        auth.signOut()
+        _authState.value = AuthState.Unauthenticated
+    }
+
+}
+
+
+sealed class AuthState{
+    object Authenticated : AuthState()
+    object Unauthenticated : AuthState()
+    object Loading : AuthState()
+    data class Error(val mesagge : String) : AuthState()
 }
