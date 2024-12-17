d2f07c6ac7353507f7e2c91f9546171c9b4ea1ee
dcuecal515 <dcuecal515@g.educaand.es>
2024-12-17 17:20:58 +0100

    Terminado cajón de navegación

--- App/app/src/main/java/com/example/gymapp/Appearance/Navegationdrawer/NavigationDrawer.kt
+++ App/app/src/main/java/com/example/gymapp/Appearance/Navegationdrawer/NavigationDrawer.kt
@@ -1,24 +1,28 @@
 package com.example.gymapp.Appearance.Navegationdrawer
 
 import android.annotation.SuppressLint
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
+import android.app.Activity
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -39,11 +43,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.gymapp.Appearance.Data.Routes
+import com.example.gymapp.Appearance.Generics.AlertDialog
 import com.example.gymapp.Appearance.Principal
 import com.example.gymapp.Appearance.Views.AboutUsContent
 import com.example.gymapp.Appearance.Views.Api.BodyPartsContent
@@ -54,6 +62,7 @@ import com.example.gymapp.Appearance.Views.SettingsContent
 import com.example.gymapp.GymApi.ViewModels.AuthViewModel
 import com.example.gymapp.GymApi.ViewModels.BodyPartsViewModel
 import com.example.gymapp.GymApi.ViewModels.ExercisesViewModel
+import com.example.gymapp.R
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -61,72 +70,49 @@ import kotlinx.coroutines.launch
 @Composable
 fun NavigationDrawer(bodyPartsViewModel: BodyPartsViewModel,exercisesViewModel: ExercisesViewModel,authViewModel: AuthViewModel){
     val navController = rememberNavController()
-
+    val context = LocalContext.current
     val showDialog = remember { mutableStateOf(false) }
 
 
     ///List of Navigation Items that will be clicked
     val items = listOf(
         NavigationItems(
-            title = "Home",
+            title = context.getString(R.string.home_text),
             selectedIcon = Icons.Filled.Home,
             unselectedIcon = Icons.Outlined.Home,
             route = Routes.Principal.route
         ),
         NavigationItems(
-            title = "Info",
+            title = context.getString(R.string.account_text),
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person,
+            route = Routes.Login.route
+        ),
+        NavigationItems(
+            title = context.getString(R.string.bodyPartsButtonTitle),
+            selectedIcon = Icons.Filled.Star,
+            unselectedIcon = Icons.Outlined.Star,
+            route = Routes.BodyPartsScreen.route
+        ),
+        NavigationItems(
+            title = context.getString(R.string.settingsTitle),
+            selectedIcon = Icons.Filled.DateRange,
+            unselectedIcon = Icons.Outlined.DateRange,
+            route = Routes.Settings.route
+        ),
+        NavigationItems(
+            title = context.getString(R.string.about_us_title),
             selectedIcon = Icons.Filled.Info,
             unselectedIcon = Icons.Outlined.Info,
             route = Routes.AboutUs.route
         ),
         NavigationItems(
-            title = "Edit",
-            selectedIcon = Icons.Filled.Edit,
-            unselectedIcon = Icons.Outlined.Edit,
-            route = Routes.BodyPartsScreen.route
-        ),
-        NavigationItems(
-            title = "Settings",
-            selectedIcon = Icons.Filled.Settings,
-            unselectedIcon = Icons.Outlined.Settings,
-            route = Routes.Settings.route
-        ),
-        NavigationItems(
-            title = "Cuenta",
-            selectedIcon = Icons.Filled.Person,
-            unselectedIcon = Icons.Outlined.Person,
-            route = Routes.Login.route
+            title = context.getString(R.string.exit_button_title),
+            selectedIcon = Icons.Filled.ExitToApp,
+            unselectedIcon = Icons.Outlined.ExitToApp,
+            route = null
         )
     )
-
-    /*Button(
-        onClick = { showDialog.value = true }, //no se puede llamar a la función directamente desde el onclick
-        shape = misFormas.small,
-        modifier = Modifier.width(250.dp)
-    ) {
-        Text(
-            text = context.getString(R.string.exit_button_title),
-            style = MaterialTheme.typography.headlineSmall,
-        )
-    }
-
-    val activity = (LocalContext.current as? Activity)
-    val imageBitmap = ImageBitmap.imageResource(R.drawable.icon)
-    // Llama al AlertDialog genérico y lo muestra
-    if (showDialog.value) {
-        AlertDialog(
-            title =  context.getString(R.string.exit_title),
-            description =  context.getString(R.string.exit_description),
-            icon = imageBitmap,
-            confirmText =  context.getString(R.string.exit_confirm),
-            dismissText = context.getString(R.string.exit_cancel),
-            confirm = {
-                activity?.finish()
-                showDialog.value = false
-            },
-            dismiss = {
-                showDialog.value = false
-            })*/
 
     //Remember Clicked item state
     var selectedItemIndex by rememberSaveable {
@@ -150,6 +136,9 @@ fun NavigationDrawer(bodyPartsViewModel: BodyPartsViewModel,exercisesViewModel: 
                         onClick = {
                             if(item.route != null) {
                                 navController.navigate(item.route)
+                            }
+                            else{
+                                showDialog.value = true
                             }
 
                             selectedItemIndex = index
@@ -206,17 +195,35 @@ fun NavigationDrawer(bodyPartsViewModel: BodyPartsViewModel,exercisesViewModel: 
                     Principal(navController, authViewModel) }
                 composable(Routes.Settings.route) { selectedItemIndex = 3
                     SettingsContent(navController, authViewModel) }
-                composable(Routes.AboutUs.route) { selectedItemIndex = 1
+                composable(Routes.AboutUs.route) { selectedItemIndex = 4
                     AboutUsContent(navController) }
                 composable(Routes.BodyPartsScreen.route) { selectedItemIndex = 2
                     BodyPartsContent(navController, bodyPartsViewModel) }
                 composable(Routes.ExercisesScreen.route) { selectedItemIndex = 2
                     ExercisesContent(navController, exercisesViewModel) }
-                composable(Routes.Login.route) { selectedItemIndex = 4
+                composable(Routes.Login.route) { selectedItemIndex = 1
                     Login(navController, authViewModel) }
-                composable(Routes.Register.route) { selectedItemIndex = 4
+                composable(Routes.Register.route) { selectedItemIndex = 1
                     Register(navController, authViewModel) }
             }
+        }
+        val activity = (LocalContext.current as? Activity)
+        val imageBitmap = ImageBitmap.imageResource(R.drawable.icon)
+        // Llama al AlertDialog genérico y lo muestra
+        if (showDialog.value) {
+            AlertDialog(
+                title =  context.getString(R.string.exit_title),
+                description =  context.getString(R.string.exit_description),
+                icon = imageBitmap,
+                confirmText =  context.getString(R.string.exit_confirm),
+                dismissText = context.getString(R.string.exit_cancel),
+                confirm = {
+                    activity?.finish()
+                    showDialog.value = false
+                },
+                dismiss = {
+                    showDialog.value = false
+                })
         }
     }
 
